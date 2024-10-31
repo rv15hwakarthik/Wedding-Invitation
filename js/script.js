9dfd499030e66a3e43fa4701a0621f55749c3b05
Vishwakarthik Ramesh <rvishwakarthik@gmail.com>
2024-10-31 14:47:19 +0530

    added door and text/color edits

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
  */
 (function ($) {
     "use strict";
-      $('.sakura-falling').sakura();
+    $('.sakura-falling').sakura();
 })(jQuery);
 
 /**
@@ -20,32 +20,32 @@
  * Listen here: https://youtu.be/X0MDALpV29s
  *
  */
-$(document).on('click', function(){
+$(document).on('click', function () {
     document.getElementById("my_audio").play();
     console.log('Shaadi me zaroor aana');
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 29, 2024 00:00:00").getTime();
+var countDownDate = new Date("Feb 03, 2025 00:00:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+var x = setInterval(function () {
 
     // Get todays date and time
     var now = new Date().getTime();
-    
+
     // Find the distance between now and the count down date
     var distance = countDownDate - now;
-    
+
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
     // Output the result in an element with id="demo"
-    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>"+ days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
-    
+    document.getElementById("time").innerHTML = "<div class='container'><div class='days block'>" + days + "<br>Days</div>" + "<div class='hours block'>" + hours + "<br>Hours</div>" + "<div class='minutes block'>" + minutes + "<br>Minutes</div>" + "<div class='seconds block'>" + seconds + "<br>Seconds</div></div>";
+
     // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
@@ -93,3 +93,10 @@ console.log(
     `%cShaadi me zaroor aana!\n\n`,
     'color: yellow; background:tomato; font-size: 24pt; font-weight: bold',
 )
+
+document.getElementById('door').addEventListener('click', function () {
+    // Open the door by adding the "door-open" class
+    document.getElementById('door').classList.add('door-open');
+    document.body.classList.remove('overflow-hidden');
+    document.getElementById('ribbon').classList.add('hide');
+});
